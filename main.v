88ffea730463892daa17f1adf8aef1647e412332
Yurii Khomenko <yykhomenko@gmail.com>
2025-04-11 09:07:03 +0200

    ref

--- main.v
+++ main.v
@@ -1,15 +1,16 @@
 import picoev
 import picohttpparser
 
-fn callback(data voidptr, req picohttpparser.Request, mut res picohttpparser.Response) {
-	res.http_ok()
-	res.header_date()
-	res.plain()
-	res.body('Hello, World!')
-	res.end()
-}
-
 fn main() {
-	mut server := picoev.new(port: 8080, cb: callback) or { panic('!!!') }
+	mut server := picoev.new(
+		port: 8080
+		cb:   fn (_ voidptr, req picohttpparser.Request, mut res picohttpparser.Response) {
+			res.http_ok()
+			res.header_date()
+			res.plain()
+			res.body('Hello, World!')
+			res.end()
+		}
+	) or { panic('Unable to create server') }
 	server.serve()
 }
