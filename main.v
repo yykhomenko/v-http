0d0c0cede11fb04bc45ddf8bd7164922b1d0abc3
Yurii Khomenko <yykhomenko@gmail.com>
2025-04-11 09:07:53 +0200

    ref

--- main.v
+++ main.v
@@ -6,7 +6,6 @@ fn main() {
 		port: 8080
 		cb:   fn (_ voidptr, req picohttpparser.Request, mut res picohttpparser.Response) {
 			res.http_ok()
-			res.header_date()
 			res.plain()
 			res.body('Hello, World!')
 			res.end()
